54040e6d216987fc341945b9e6a0b14817d913eb
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-11 00:23:05 +0100

    Python Program on basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10 
+number2 = 5 
+#Addition of two numbers 
+sum_result = number1 + number2 
+
+#Difference of the two numbers 
+difference_result  = number1 - number2 
+
+#Multiplication of the two numbers 
+product_result = number1 * number2 
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
+
+
