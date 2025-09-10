fec5345a459a7bfd7f8ceb2e33b9ebff0e897c55
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-11 00:34:41 +0100

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#A python programto calculate simple interest 
+
+principal = 1000
+rate = float(0.05)
+time = 3 
+
+#Formulating simple interest 
+interest = principal * rate * time 
+
+print(f"The simple interest is: {interest}")
