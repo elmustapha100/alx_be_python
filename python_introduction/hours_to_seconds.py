969960309da52096e8fd19aa41448e116c687f24
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-11 07:24:38 +0100

    Changed the seconds value according to the instruction

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 # A basic program that convert hours to seconds
 hours = 2 
-seconds = hours * (60 ** 2) # Changing hours to seconds 
+seconds = hours * 3600 # Changing hours to seconds 
 
 print(f"{hours} hour(s) is {seconds} seconds. ")
 
