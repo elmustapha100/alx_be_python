7f5c53b14c1258ccf13961416d83acc0c787b479
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-11 07:42:22 +0100

    Made changes in the user input prompt

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
 # A basic python program that calculate the user’s age in a future year.
 
-user_age = int(input("Enter your current age : \n")) # getting input age from user 
+user_age = int(input("How old are you ?")) # getting input age from user 
 
 age = user_age + 27 
 
