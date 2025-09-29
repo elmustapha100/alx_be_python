79cf80960eac2dff39e922ae8e02daac1ad818bb
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-29 12:05:10 +0100

    Final commit on arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,7 +14,7 @@ def perform_operation(num1, num2, operation):
     
     elif operation == "divide" : 
         if num2 == 0 : 
-         return "Cannot divide by zero"
+         return "Error: Cannot divide by zero"
         else : 
           return num1 / num2
      
