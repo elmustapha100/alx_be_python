9c337fdfe525f75f4468c58c5b8061f6a4b6485b
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-29 10:39:21 +0100

    Made some changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -15,7 +15,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "divide" : 
         if num2 == 0 : 
          return "Cannot divide by zero"
-        return num1 / num2
+        else : 
+          return num1 / num2
      
     else :
         return "Error: Invalid operation" 
