815d7402ca64a6a4ea579091ec1e6c5d54918bd5
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-29 08:28:30 +0100

    First commit on functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,11 +13,10 @@ def perform_operation(num1, num2, operation):
         return num1 * num2 
     
     elif operation == "divide" : 
-        if num2 != 0 : 
-            return num1 / num2 
-        else : 
-            return "Error: Division by zero is not allowed"
-         
+        if num2 == 0 : 
+         return "Cannot divide by zero"
+        return num1 / num2
+     
     else :
         return "Error: Invalid operation" 
 
