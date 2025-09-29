6a2a12681f607d7f59a877ee49a0cae9781f3793
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-09-29 08:21:57 +0100

    Functions on arithmetic operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,24 @@
+# Arithmetic Operations Function
+
+def perform_operation(num1, num2, operation): 
+    """A function that performs arithmetic operations ontwo numerical values."""
+
+    if operation == "add" : 
+        return num1 + num2 
+    
+    elif operation == "subtract" :
+        return num1 - num2 
+    
+    elif operation == "multiply" : 
+        return num1 * num2 
+    
+    elif operation == "divide" : 
+        if num2 != 0 : 
+            return num1 / num2 
+        else : 
+            return "Error: Division by zero is not allowed"
+         
+    else :
+        return "Error: Invalid operation" 
+
+    
