9300c3377f8ed89619f238d1e62e61ead683297c
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-10-16 13:42:40 +0100

    Final commit on Class & static methods on OOP

--- oop/main.py
+++ oop/main.py
@@ -3,12 +3,12 @@ from class_static_methods_demo import Calculator
 #Implementing the static method 
 def main() : 
     sum_result = Calculator.add(10,5)
-    print(f"The sum is : {sum_result}")
+    print(f"The sum is: {sum_result}")
     
     #Implementing the class method 
 
     product_result = Calculator.multiply(10,5)
-    print(f"The product is : {product_result}")
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__" : 
     main() 
