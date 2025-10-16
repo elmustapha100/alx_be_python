559ca8e9c2a5d1853d9da5a93a88bc5691d97b36
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-10-16 12:25:53 +0100

    Initial commit

---
+++ oop/main.py
@@ -0,0 +1,16 @@
+from class_static_methods_demo import Calculator 
+
+#Implementing the static method 
+def main() : 
+    sum_result = Calculator.add(10,5)
+    print(f"The sum is : {sum_result}")
+    
+    #Implementing the class method 
+
+    product_result = Calculator.multiply(10,5)
+    print(f"The product is : {product_result}")
+
+if __name__ == "__main__" : 
+    main() 
+
+ 
