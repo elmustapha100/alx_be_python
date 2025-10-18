51131c9f768006d48af6cf676421051b94a42ac4
EL Mustapha <elmustaphaabimbola005@gmail.com>
2025-10-18 13:08:26 +0100

    Final commit

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor: Initializes a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    def __del__(self):
+        """Destructor: Prints a message when the Book instance is deleted."""
+        print(f"Deleting {self.title}")
+    
+    def __str__(self):
+        """String Representation: Returns a user-friendly string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        """Official Representation: Returns a string that can recreate the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
